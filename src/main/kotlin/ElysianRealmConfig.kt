7f46b7c49c48f33e002ebe233815c1569deb6393
Misaki <1226594277@qq.com>
2023-01-09 13:27:57 +0800

    v1.3.1; 修改攻略图命名方式

--- src/main/kotlin/ElysianRealmConfig.kt
+++ src/main/kotlin/ElysianRealmConfig.kt
@@ -10,193 +10,198 @@ object ElysianRealmConfig : AutoSavePluginConfig("ElysianRealmConfig") {
     val ElysianRealmConfig: Map<String, Set<String>> by value(
 
         mapOf(
-            "人之律者" to setOf(
+            "Human" to setOf(
                 "人律乐土",
                 "爱律乐土"
             ),
-            "空之律者" to setOf(
+            "Void" to setOf(
                 "空律乐土",
                 "女王乐土"
             ),
-            "格蕾修" to setOf(
+            "Starry" to setOf(
                 "繁星乐土",
                 "格蕾修乐土",
                 "绘世乐土"
             ),
-            "阿波尼亚" to setOf(
+            "Disciplinary" to setOf(
                 "阿波尼亚乐土",
                 "戒律乐土"
             ),
-            "伊甸" to setOf(
+            "Golden" to setOf(
                 "伊甸乐土",
                 "黄金乐土"
             ),
-            "帕朵菲莉丝" to setOf(
+            "Felis" to setOf(
                 "帕朵乐土",
                 "猫猫乐土",
                 "菲利丝乐土"
             ),
-            "伊甸" to setOf(
-                "伊甸乐土",
-                "黄金乐土"
-            ),
-            "缭乱星棘" to setOf(
+            "Oven" to setOf(
                 "火塔乐土",
                 "烤箱乐土",
                 "星棘乐土",
                 "星塔乐土"
             ),
-            "天元骑英" to setOf(
+            "Palatinus" to setOf(
                 "天元乐土",
                 "泥头鹅乐土"
             ),
-            "次生银翼" to setOf(
+            "Silverwing" to setOf(
                 "大鸭鸭乐土",
                 "次生银翼乐土"
             ),
-            "卡萝尔" to setOf(
+            "Carol" to setOf(
                 "卡萝尔乐土"
             ),
-            "伊甸" to setOf(
-                "伊甸乐土",
-                "黄金乐土"
-            ),
-            "梅比乌斯" to setOf(
+            "Lnfinite" to setOf(
                 "梅比乌斯乐土",
                 "蛇蛇乐土"
             ),
-            "渡鸦" to setOf(
+            "Raven" to setOf(
                 "渡鸦乐土"
             ),
-            "雷之律者" to setOf(
+            "Thunder" to setOf(
                 "雷律乐土"
             ),
-            "雷律平A流" to setOf(
+            "Thunder_Attack" to setOf(
                 "雷律平A流"
             ),
-            "雷律天罚流" to setOf(
+            "Thunder_Punishment" to setOf(
                 "雷律天罚流"
             ),
-            "失落迷迭" to setOf(
+            "Rosemary" to setOf(
                 "迷迭乐土"
             ),
-            "狂热蓝调Δ" to setOf(
+            "Delta" to setOf(
                 "萝莎莉娅乐土",
                 "粉毛乐土",
                 "德尔塔乐土"
             ),
-            "不灭星锚" to setOf(
+            "Anchora" to setOf(
                 "星锚乐土",
                 "星猫乐土"
             ),
-            "爱莉希雅" to setOf(
+            "Elysia" to setOf(
                 "爱莉乐土",
                 "爱莉希雅乐土"
             ),
-            "薪炎之律者" to setOf(
+            "Flamescion" to setOf(
                 "炎律乐土",
                 "薪炎乐土"
             ),
-            "识之律者" to setOf(
+            "Sentience" to setOf(
                 "识律乐土",
                 "识宝乐土"
             ),
-            "魇夜星渊" to setOf(
+            "Starchasm" to setOf(
                 "奶希乐土",
                 "冰希乐土"
             ),
-            "菲谢尔" to setOf(
+            "Fischl" to setOf(
                 "皇女乐土",
                 "菲谢尔乐土"
             ),
-            "彼岸双生" to setOf(
+            "TwinSeele" to setOf(
                 "双生乐土",
                 "黑希乐土"
             ),
-            "辉骑士 · 月魄" to setOf(
+            "Excelsis" to setOf(
                 "幽兰戴尔乐土",
                 "月魄乐土",
                 "呆鹅乐土"
             ),
-            "暮光骑士 · 月煌" to setOf(
+            "Twilight" to setOf(
                 "月煌乐土",
                 "紫苑乐土"
             ),
-            "圣仪装 · 今样" to setOf(
+            "Kallen" to setOf(
                 "今样乐土",
                 "卡莲乐土"
             ),
-            "御神装 · 勿忘" to setOf(
+            "Moment" to setOf(
                 "勿忘乐土",
                 "机八乐土",
                 "冰八乐土",
                 "八重樱乐土"
             ),
-            "苍骑士 · 月魂" to setOf(
+            "Refrigerator" to setOf(
                 "月魂乐土",
                 "冰箱乐土",
                 "冰塔乐土"
             ),
-            "破晓强袭" to setOf(
+            "Bladestrike" to setOf(
                 "强袭乐土"
             ),
-            "月下初拥" to setOf(
+            "LunaKindred" to setOf(
                 "月下乐土"
             ),
-            "理之律者" to setOf(
+            "Reason" to setOf(
                 "理律乐土",
                 "摩托鸭乐土",
                 "车车乐土"
             ),
-            "女武神 · 荣光" to setOf(
+            "Gloria" to setOf(
                 "荣光乐土"
             ),
-            "人律蓄力流" to setOf(
+            "Human_Branch" to setOf(
                 "人律蓄力流",
                 "人律纯蓄流",
                 "人律蓄力"
             ),
-            "李素裳" to setOf(
+            "Susang" to setOf(
                 "李素裳乐土",
                 "月痕乐土"
             ),
-            "维尔薇" to setOf(
+            "Helical" to setOf(
                 "维尔薇乐土",
                 "维尔微乐土",
                 "v2v乐土"
             ),
-            "女王大招流" to setOf(
+            "Void_Skill" to setOf(
                 "女王大招流",
                 "空律大招流"
             ),
-            "真红骑士 · 月蚀" to setOf(
+            "Eclipse" to setOf(
                 "姬子乐土",
                 "真红乐土",
                 "月蚀乐土"
             ),
-            "时帆旅人" to setOf(
+            "Cabbage" to setOf(
                 "包菜乐土",
                 "爱酱乐土",
                 "爱衣乐土"
             ),
-            "真理之律者" to setOf(
+            "Truth" to setOf(
                 "真理乐土",
                 "真律乐土",
                 "真鸭乐土",
                 "真理之律者乐土"
             ),
-            "第六夜想曲" to setOf(
+            "Serenade" to setOf(
                 "冰卡乐土",
                 "s卡乐土",
                 "S卡乐土",
                 "怪盗乐土",
                 "舞卡乐土"
             ),
-            "真律武器流" to setOf(
+            "Truth_Weapon" to setOf(
                 "真理武器流",
                 "真律武器流",
                 "真鸭武器流",
                 "真理之律者武器流"
+            ),
+            "Eclipse_Branch" to setOf(
+                "姬子蓄力流",
+                "真红蓄力流",
+                "月蚀蓄力流"
+            ),
+            "first" to setOf(
+                "始源乐土",
+                "始源之律者乐土"
+            ),
+            "Finally" to setOf(
+                "终焉乐土",
+                "终焉之律者乐土"
             )
         )
     )
